eb4367c49eb3f4ccc8ca396200c584e5af6b7b93
KelvinHYYip <74517117+KelvinHYYip@users.noreply.github.com>
2022-09-21 22:09:11 +1000

    Initialize ChelseaToner

---
+++ rnaseq/run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH -p long
+#SBATCH --job-name=HEPG2_rna_seq
+#SBATCH --mail-type=END,FAIL
+<<<<<<< HEAD
+#SBATCH --mail-user=chelsea.toner@colorado.edu
+=======
+#SBATCH --mail-user=michael.smallegan@colorado.edu
+>>>>>>> origin/in_class
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=6gb
+#SBATCH --time=20:00:00
+#SBATCH --output=nextflow.out
+#SBATCH --error=nextflow.err
+
+pwd; hostname; date
+echo "Here we go You've requested $SLURM_CPUS_ON_NODE core."
+
+module load singularity/3.1.1
+
+nextflow run nf-core/rnaseq -r 3.0 \
+-resume \
+-profile singularity \
+--input design.csv \
+--aligner star_salmon \
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+--fasta /scratch/Shares/rinnclass/data/genomes/GRCh38.p13.genome.fa \
+--gtf /scratch/Shares/rinnclass/data/genomes/gencode.v32.annotation.gtf \
+=======
+--fasta /Shares/rinn_class/data/genomes/human/gencode/v32/GRCh38.p13.genome.fa \
+--gtf /Shares/rinn_class/data/genomes/human/gencode/v32/gencode.v32.annotation.gtf \
+>>>>>>> origin/in_class
+=======
+--fasta /Shares/rinn_class/data/genomes/human/gencode/v32/GRCh38.p13.genome.fa \
+--gtf /Shares/rinn_class/data/genomes/human/gencode/v32/gencode.v32.annotation.gtf \
+>>>>>>> origin/in_class
+--gencode \
+--email chelsea.toner@colorado.edu \
+=======
+--fasta /scratch/Shares/rinnclass/data/genomes/GRCh38.p13.genome.fa \
+--gtf /scratch/Shares/rinnclass/data/genomes/gencode.v32.annotation.gtf \
+--gencode \
+--email michael.smallegan@colorado.edu \
+>>>>>>> origin/in_class
+-c nextflow.config
+
+date
